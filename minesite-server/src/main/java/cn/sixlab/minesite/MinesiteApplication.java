54ebb651a0befa3a71f37a92f0d3968f08473ed3
nianqinianyi <nianqinianyi@163.com>
2020-01-07 00:03:35 +0800

    使用 Spring Boot 初始化项目

---
+++ minesite-server/src/main/java/cn/sixlab/minesite/MinesiteApplication.java
@@ -0,0 +1,13 @@
+package cn.sixlab.minesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinesiteApplication.class, args);
+	}
+
+}
